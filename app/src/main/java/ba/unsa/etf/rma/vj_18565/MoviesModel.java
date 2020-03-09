ef0a7b49f0049a8dc8a2b5fe555085e745f111c3
aalialzaya1 <KimJongIn88!>
2020-03-09 10:30:57 +0100

    whaddup

---
+++ app/src/main/java/ba/unsa/etf/rma/vj_18565/MoviesModel.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rma.vj_18565;
+
+import java.util.ArrayList;
+
+public class MoviesModel {
+    private static Movie movie = new Movie("Mulan", "animated", "24.03.2020",
+            "https://www.imdb.com/title/tt4566758/","A young Chinese " +
+            "maiden disguises herself as a male warrior in order to save her father." +
+            " A live-action feature film based on Disney's 'Mulan.' ");
+    private ArrayList<Movie> movies = new ArrayList<Movie>();
+}
